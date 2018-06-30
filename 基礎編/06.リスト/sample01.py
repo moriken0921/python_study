cdf9ce4af2418159b15741f791f8f10c43cce8d6
Kento Mori <kento.mori0921@gmail.com>
2018-06-30 12:49:30 +0900

    追加

---
+++ 基礎編/06.リスト/sample01.py
@@ -0,0 +1,7 @@
+test_list_1 = ['python', '-', 'izm', '.', 'com']
+print(test_list_1)
+
+print('--------------------------------')
+
+for i in test_list_1:
+    print(i)
---
+++ 基礎編/06.リスト/sample03.py
@@ -0,0 +1,13 @@
+test_list_1 = ['python', 'izm', 'com']
+print(test_list_1)
+
+print('--------------------------------')
+
+test_list_1.insert(1, '-')
+test_list_1.insert(3, '.')
+
+print(test_list_1)
+
+test_list_1.insert(0, 'http://www.')
+
+print(test_list_1)
