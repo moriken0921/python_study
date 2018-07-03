dd2acd286a1fc2564b3404b819a58c104822fa36
Kento Mori <kento.mori0921@gmail.com>
2018-07-03 23:17:38 +0900

    追加

---
+++ 基礎編/06.リスト/sample04.py
@@ -0,0 +1,8 @@
+test_list_1 = ['1', '2', '3', '2', '1']
+print(test_list_1)
+
+print('--------------------------------')
+
+test_list_1.remove('2')
+
+print(test_list_1)
---
+++ 基礎編/06.リスト/sample05.py
@@ -0,0 +1,10 @@
+test_list_1 = ['1', '2', '3', '2', '1']
+print(test_list_1)
+
+print('--------------------------------')
+
+print(test_list_1.pop(1))
+print(test_list_1)
+
+print(test_list_1.pop())
+print(test_list_1)
