99f6b6e56227af8ed412025f664747a6f684dc39
Kento Mori <kento.mori0921@gmail.com>
2018-07-22 22:40:57 +0900

    追加

---
+++ 基礎編/08.セット/sample04.py
@@ -0,0 +1,6 @@
+test_set_1 = set()
+
+test_set_1.add('python')
+test_set_1.update({'-', 'izm', '.', 'com'})
+
+print(test_set_1)
---
+++ 基礎編/08.セット/sample05.py
@@ -0,0 +1,6 @@
+test_set_1 = frozenset({'python', '-', 'izm', '.', 'com'})
+
+# test_set_1.remove('-')
+# test_set_1.discard('.')
+
+print(test_set_1)
