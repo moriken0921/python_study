552e66af7bb2cc0f67e87c2a4071270a4581bdf5
Kento Mori <kento.mori0921@gmail.com>
2018-08-27 01:03:30 +0900

    追加

---
+++ 基礎編/12.コマンドライン引数/test_argv.py
@@ -0,0 +1,10 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+args = sys.argv
+
+print(args)
+print('第1引数:' + args[1])
+print('第2引数:' + args[2])
+print('第3引数:' + args[3])
